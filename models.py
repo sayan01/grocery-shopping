0df339f4e5967e769ba612de33e6113565fc8ea0
Sayan Ghosh <sgic2001@gmail.com>
2024-01-19 11:46:50 +0530

    flash and auth

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy(app)
 
@@ -50,3 +51,10 @@ class Order(db.Model):
 
 with app.app_context():
     db.create_all()
+    # if admin exists, else create admin
+    admin = User.query.filter_by(is_admin=True).first()
+    if not admin:
+        password_hash = generate_password_hash('admin')
+        admin = User(username='admin', passhash=password_hash, name='Admin', is_admin=True)
+        db.session.add(admin)
+        db.session.commit()
--- routes.py
+++ routes.py
@@ -1,11 +1,16 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, session
 from app import app
 from models import db, User, Category, Product, Cart, Transaction, Order
 from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # user_id in session
+    if 'user_id' in session:
+        return render_template('index.html')
+    else:
+        flash('Please login to continue')
+        return redirect(url_for('login'))
 
 @app.route('/login')
 def login():
@@ -30,6 +35,8 @@ def login_post():
         flash('Incorrect password')
         return redirect(url_for('login'))
     
+    session['user_id'] = user.id
+    flash('Login successful')
     return redirect(url_for('index'))
 
 
