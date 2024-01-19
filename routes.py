1dfb32cbd7c411602555ca6232e5b3c74a76b925
Sayan Ghosh <sgic2001@gmail.com>
2024-01-19 10:52:11 +0530

    Add controllers for register and login

--- routes.py
+++ routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from app import app
+from models import db, User, Category, Product, Cart, Transaction, Order
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route('/')
 def index():
@@ -9,6 +11,56 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not username or not password:
+        flash('Please fill out all fields')
+        return redirect(url_for('login'))
+    
+    user = User.query.filter_by(username=username).first()
+    
+    if not user:
+        flash('Username does not exist')
+        return redirect(url_for('login'))
+    
+    if not check_password_hash(user.passhash, password):
+        flash('Incorrect password')
+        return redirect(url_for('login'))
+    
+    return redirect(url_for('index'))
+
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirm_password')
+    name = request.form.get('name')
+
+    if not username or not password or not confirm_password:
+        flash('Please fill out all fields')
+        return redirect(url_for('register'))
+    
+    if password != confirm_password:
+        flash('Passwords do not match')
+        return redirect(url_for('register'))
+    
+    user = User.query.filter_by(username=username).first()
+
+    if user:
+        flash('Username already exists')
+        return redirect(url_for('register'))
+    
+    password_hash = generate_password_hash(password)
+    
+    new_user = User(username=username, passhash=password_hash, name=name)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
